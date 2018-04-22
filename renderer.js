e676a5e327445e4201701448db7a97759ad44e52
David Yesil <David.Yesil@hs-augsburg.de>
2018-04-23 01:03:59 +0200

    added safe download

--- renderer.js
+++ renderer.js
@@ -1,17 +1,30 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const con = require('electron').remote.getGlobal('console')
+var nodeConsole = require('console');
+var con = new nodeConsole.Console(process.stdout, process.stderr);
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 const downloadButton = document.getElementById('download');
 const urlInput = document.getElementById('url_input');
 
+const manager = {};
 
 downloadButton.addEventListener('click', () => {
-
-
-  ytdl(urlInput.value)
-  .pipe(fs.createWriteStream('video.mp4'));
+  if (!manager[urlInput.value]) {
+    manager[urlInput.value] = true;
+    const video = ytdl(urlInput.value);
+    video.on('progress', (chunkLength, downloaded, totalLength) => {
+      //con.log(chunkLength, downloaded, totalLength);
+      if (downloaded == totalLength) {
+        con.log('fin');
+        manager[urlInput.value] = false;
+      }
+    });
+    video.on('info', (chunkLength, downloaded) => {
+      con.log(chunkLength, downloaded)
+    });
+    video.pipe(fs.createWriteStream('video.mp4'));
+  }
 });
--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-
-function download() {
-  ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-  .pipe(fs.createWriteStream('video.mp4'));
-}
-
-download();
